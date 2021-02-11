7bb8b998b5ce5055bddff76cee8ac7ebf9ce7f14
Michael Andersen <mian@kfst.dk>
2021-02-11 12:35:11 +0100

    Update version to 0.1.0

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='DatasetPathManager',
-    version='0.0.1',
+    version='0.1.0',
     description='Small class to handle a image dataset',
     author='Michael Andersen',
     author_email='gosuckadeadcow+github@gmail.com',
