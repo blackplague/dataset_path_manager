53ff872f5c5c465ecfa5d09939ba3600895938b9
Michael Andersen <mian@kfst.dk>
2021-07-01 08:03:13 +0200

    Update setup.py according to [0]
      - [0]: https://github.com/navdeep-G/samplemod/

--- setup.py
+++ setup.py
@@ -1,5 +1,8 @@
 from setuptools import setup, find_packages
 
+with open('README.md') as f:
+    readme = f.read()
+
 setup(
     name='DatasetPathManager',
     version='0.1.0',
@@ -7,7 +10,5 @@ setup(
     author='Michael Andersen',
     author_email='gosuckadeadcow+github@gmail.com',
     url='https://github.com/blackplague/dataset_path_manager',
-    packages=find_packages(include=['datasetpathmanager', 'datasetpathmanager.*']),
-    setup_requires=['pytest-runner'],
-    tests_require=['pytest'],
+    packages=find_packages(exclude=('tests')),
 )
